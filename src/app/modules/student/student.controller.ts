99663c4b42f30bc33642b4cd1996bd782039ed65
fatima arifeen <fatima.arifeen24@gmail.com>
2025-01-01 20:56:07 +0600

    post and get method done

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -4,6 +4,7 @@ import { StudentServices } from "./student.service";
 const createStudent= async (req: Request, res: Response)=>{
 try{
     const {student: studentData} = req.body;
+  
 const result = await StudentServices.createStudentIntoDB(studentData);
 
 res.status(200).json({
@@ -16,6 +17,36 @@ res.status(200).json({
 }
 };
 
+const getAllStudents = async(req: Request, res: Response) =>{
+  try{
+    const result = await StudentServices.getAllStudentFromDB();
+
+    res.status(200).json({
+        success: true,
+        message:"students are retrieved successfully",
+        data: result,
+    });
+  }catch(err){
+   console.log(err)
+  }
+}
+const getSingleStudent = async(req: Request, res: Response) =>{
+  try{
+    const {studentId} = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+        success: true,
+        message:"students is retrieved successfully",
+        data: result,
+    });
+  }catch(err){
+   console.log(err)
+  }
+}
+
 export const StudentControllers = {
     createStudent,
+    getAllStudents,
+    getSingleStudent,
 }
